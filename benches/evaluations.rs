f8b498c2dafc95b57828187f68963759cdd9abd4
Pratyush Mishra <pratyushmishra@berkeley.edu>
2022-01-13 21:16:35 -0800

    Speed up benchmarks

--- benches/evaluations.rs
+++ benches/evaluations.rs
@@ -16,18 +16,18 @@ fn evaluations(c: &mut Criterion) {
     group.sampling_mode(SamplingMode::Flat);
     group.sample_size(10);
 
-    for log_n in 1..=P::LOG_N {
+    for log_n in 10..=P::LOG_N {
         group.bench_with_input(BenchmarkId::new("ECFFT", log_n), &log_n, |b, _| {
             let coeffs: Vec<F> = (0..1 << log_n).map(|_| rng.gen()).collect();
             let poly = DensePolynomial { coeffs };
             b.iter(|| precomputation.evaluate_over_domain(&poly));
         });
-        // group.bench_with_input(BenchmarkId::new("Naive", log_n), &log_n, |b, _| {
-        //     let coeffs: Vec<F> = (0..1 << log_n).map(|_| rng.gen()).collect();
-        //     let poly = DensePolynomial { coeffs };
-        //     let coset = P::sub_coset(P::LOG_N - log_n);
-        //     b.iter(|| coset.iter().map(|x| poly.evaluate(x)).collect::<Vec<_>>());
-        // });
+        group.bench_with_input(BenchmarkId::new("Naive", log_n), &log_n, |b, _| {
+            let coeffs: Vec<F> = (0..1 << log_n).map(|_| rng.gen()).collect();
+            let poly = DensePolynomial { coeffs };
+            let coset = P::sub_coset(P::LOG_N - log_n);
+            b.iter(|| coset.iter().map(|x| poly.evaluate(x)).collect::<Vec<_>>());
+        });
         group.bench_with_input(BenchmarkId::new("Classic", log_n), &log_n, |b, _| {
             let coeffs: Vec<Fr> = (0..1 << log_n).map(|_| rng.gen()).collect();
             let poly = DensePolynomial { coeffs };
